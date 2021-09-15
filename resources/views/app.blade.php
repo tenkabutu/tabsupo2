9c348c3959079ca00791cfe10dedbc18e7f6a649
tenkabutu <tenkabutu@gmail.com>
2021-09-15 16:05:47 +0900

    no message

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,13 +23,13 @@
     <meta charset="utf-8">
     <title>LaravelSPA</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('/public/css/app.css') }}" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
 <div id="app">
    <app></app>
 </div>
-<script src="{{ mix('/js/app.js') }}"></script>
+<script src="{{ mix('/public/js/app.js') }}"></script>
 </body>
 </html>
